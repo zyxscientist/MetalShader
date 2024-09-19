13a6520e86768c59077c2ba95db23bf2b1987dae
PeterZ <peter.zhu@yff.com>
2024-09-19 10:16:50 +0800

    Initial Commit

---
+++ MetalShader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalShader
+//
+//  Created by admin on 2024/9/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalShader/MetalShaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalShaderApp.swift
+//  MetalShader
+//
+//  Created by admin on 2024/9/19.
+//
+
+import SwiftUI
+
+@main
+struct MetalShaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
