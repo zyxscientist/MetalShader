00958c60b6d3b3fd3b45c257f2c442aea6d12916
PeterZ <peter.zhu@yff.com>
2024-09-29 11:27:23 +0800

    mesh animate

---
+++ MetalShader/MeshAnimate.swift
@@ -0,0 +1,18 @@
+//
+//  MeshAnimate.swift
+//  MetalShader
+//
+//  Created by admin on 2024/9/29.
+//
+
+import SwiftUI
+
+struct MeshAnimate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeshAnimate()
+}
